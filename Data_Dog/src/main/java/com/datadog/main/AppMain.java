4defa7bde6ee78541b19b11a4f135f4202f208c9
muthamizhan <muthamizhan@razorthink.net>
2016-09-28 13:10:57 +0530

    Added main class

---
+++ Data_Dog/src/main/java/com/datadog/main/AppMain.java
@@ -0,0 +1,14 @@
+package com.datadog.main;
+
+public class AppMain {
+
+	public static void main( String[] args ) throws Exception
+	{
+		while( true )
+		{
+			System.out.println("RUNNING");
+			Thread.sleep(10000);
+		}
+	}
+
+}
